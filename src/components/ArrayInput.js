28949a979afacbda97acc12bc623c664feedc76d
Anton Skoting <skotinganton@gmail.com>
2021-11-25 11:32:25 +0100

    Update 21-11-25 11:25

--- src/components/ArrayInput.js
+++ src/components/ArrayInput.js
@@ -3,7 +3,10 @@ import React from "react";
 function ArrayInput({ min, max, values, onChange }) {
 
     function onChangeNum(event, key) {
-        const newValue = Number.parseInt(event.target.value);
+        let newValue = Number.parseInt(event.target.value);
+        if(!newValue) newValue = min;
+        newValue = Math.max(min, newValue);
+        newValue = Math.min(max, newValue);
         onChange(values.map((value, index) => {
             if(index === key) return newValue;
             else return value;
@@ -13,7 +16,7 @@ function ArrayInput({ min, max, values, onChange }) {
     return (
         <div className="ArrayInput">
             {values.map((value, index) => {
-                return <input type="number" onChange={event => onChangeNum(event, index)} style={{ width: "2rem", textAlign: 'right', backgroundColor: '#FFF' }} min={min} max={max} value={value} key={index} />
+                return <input type="number" onChange={event => onChangeNum(event, index)} style={{ width: "2rem", textAlign: 'right', backgroundColor: '#FFF' }} value={value} key={index} />
             })}
         </div>
     );
