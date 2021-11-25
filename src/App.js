b143df59afcdc85903efa1f21847210ff9c4906f
Anton Skoting <skotinganton@gmail.com>
2021-11-25 10:54:01 +0100

    Update 21-11-25 10:55

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import PointCalculator from './components/PointCalculator';
 
 function App() {
   return (
-    <div className="App" style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-      <h1 className="header" style={{margin: '0 0 30px 0'}} >
-        Point-by Calculator
-      </h1>
-      <h6 className="header" style={{fontWeight: 'normal'}}>
-        <b>Abileties:</b><br/>Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma.
-      </h6>
-      <PointCalculator />
+    <div>
+      <div className="App" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <h1 className="header" style={{ margin: '0 0 30px 0' }} >
+          Point-by Calculator
+        </h1>
+        <h6 className="header" style={{ fontWeight: 'normal' }}>
+          <b>Abileties:</b><br />Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma.
+        </h6>
+        <PointCalculator />
+      </div>
+      <h3 className="credit">Art by Tyler Jacobson</h3>
     </div>
   );
 }
